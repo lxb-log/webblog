37d582cec31e638455123668fbf4dd3c77c5002f
lxb <790898628@qq.com>
2021-05-14 17:58:35 +0800

    滚动条

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -14,13 +14,19 @@
 			</div>
 		</div>
 		<div class="content-middle">
-			<div class="timestamp-middle box">
-				<div class="block">
+			<div class="timestamp-middle box" style="">
+				<div class="block" >
 					<el-timeline>
 						<el-timeline-item timestamp="2018/4/12" placement="top">
 							<el-card>
 								<h4>更新 Github 模板</h4>
 								<p>王小虎 提交于 2018/4/12 20:46</p>
+							</el-card>
+						</el-timeline-item>
+						<el-timeline-item timestamp="2018/4/3" placement="top">
+							<el-card>
+								<h4>更新 Github 模板</h4>
+								<p>王小虎 提交于 2018/4/3 20:46</p>
 							</el-card>
 						</el-timeline-item>
 						<el-timeline-item timestamp="2018/4/3" placement="top">
@@ -111,7 +117,7 @@
 	.content .content-left {
 		width:25%;
 		height:100%;
-		background: #1a5acd;
+		background: #fd827d;
 		/* padding:0px 10px 0px 0px;  上右下左 */
 	}
 
@@ -119,6 +125,7 @@
 		width:50%;
 		height:100%;
 		background: #72cde9;
+		
 	}
 
 	.content .content-right {
@@ -152,7 +159,26 @@
 	}
 	.content .content-middle .timestamp-middle {
 		background: #89dcf5;
-		/* height: 95%; */
+		height: 85%;
 		padding: 30px;   /*上右下左*/
+		overflow: auto;  /* 滚动条 */
+	}
+
+	/*-------滚动条整体样式----*/
+	.content .content-middle .timestamp-middle::-webkit-scrollbar {
+		width:8px;
+		height:8px;
+	}
+	/*滚动条里面小方块样式*/
+	.content .content-middle .timestamp-middle::-webkit-scrollbar-thumb {
+		border-radius:100px;
+		-webkit-box-shadow:inset 0 0 5px rgba(0,0,0,0.2);
+		background:rgb(144, 247, 149);
+	}
+	/*滚动条里面轨道样式*/
+	.content .content-middle .timestamp-middle::-webkit-scrollbar-track {
+		-webkit-box-shadow:inset 0 0 5px rgba(0,0,0,0.2);
+		border-radius:0;
+		background:rgba(255, 97, 97, 0.1);
 	}
 </style>
